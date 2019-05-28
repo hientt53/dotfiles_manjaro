20191ff0740ec34d32ba43c1dbf9834b238a9015
p4fun <hientt53@gmail.com>
2019-05-28 13:40:06 +0700

    enable font icon

--- .zsh/powerlevel9k.zsh
+++ .zsh/powerlevel9k.zsh
@@ -1,3 +1,14 @@
 POWERLEVEL9K_PROMPT_ON_NEWLINE=true
-POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(context dir virtualenv vcs)
+#POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(context dir virtualenv vcs)
+POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(
+    os_icon
+    dir
+    virtualenv
+    vcs
+)
 POWERLEVEL9K_MODE='nerdfont-complete'
+
+
+source $HOME/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-bhilburn-SLASH-powerlevel9k/powerlevel9k.zsh-theme
+fpath+=( /home/p4fun/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-bhilburn-SLASH-powerlevel9k )
+
