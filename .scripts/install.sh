14c412444623f0df979115875ea502dbaabddc0c
hientt53 <hientt53@gmail.com>
2020-05-15 22:00:40 +0700

    add scripts

---
+++ .scripts/install.sh
@@ -0,0 +1,17 @@
+yay -S antibody \
+	fzf \
+       	the_silver_searcher \
+	exa \
+	vim-plug \
+	gvim \
+	ctags \
+	tmux \
+	termite \
+	xcwd-git \
+	sc-im \
+	rofi \
+	zsh \
+	xcape \
+	xorg-xmodmap \
+	autorandr \
+	xflux-gui-git
