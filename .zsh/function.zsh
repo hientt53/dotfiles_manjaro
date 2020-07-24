833e5e998e89422f926d5b20dd2f7c6945eff257
hientt53 <hientt53@gmail.com>
2020-07-24 12:18:20 +0700

    add lazzy git function

--- .zsh/function.zsh
+++ .zsh/function.zsh
@@ -15,3 +15,15 @@ function prompt()
 # gitignore generate function
 function gi() { curl -sLw n https://www.gitignore.io/api/$@ ;}
 
+# lazygit
+lg()
+{
+    export LAZYGIT_NEW_DIR_FILE=~/.lazygit/newdir
+
+    lazygit "$@"
+
+    if [ -f $LAZYGIT_NEW_DIR_FILE ]; then
+            cd "$(cat $LAZYGIT_NEW_DIR_FILE)"
+            rm -f $LAZYGIT_NEW_DIR_FILE > /dev/null
+    fi
+}
